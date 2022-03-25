74bd86565f1b81a5889106e3db903bd4664e1c59
KandaTeach <www.keneth.com.ph@gmail.com>
2022-03-26 03:05:22 +0800

    exception handling for login form

--- TripleJPMVPLibrary/Presenter/LogInFrmPresenter.cs
+++ TripleJPMVPLibrary/Presenter/LogInFrmPresenter.cs
@@ -19,6 +19,12 @@ namespace TripleJPMVPLibrary.Presenter
         public bool LogInConfirmation()
         {
             AdministratorService administratorService = new AdministratorService();
+
+            if (string.IsNullOrEmpty(_logIn.UserName) || string.IsNullOrEmpty(_logIn.PassWord))
+            {
+                throw new ArgumentException(" either the username or password is empty ");
+            }
+
             bool result = administratorService.IsRegistered(_logIn.UserName, _logIn.PassWord);
             return result;
         }
