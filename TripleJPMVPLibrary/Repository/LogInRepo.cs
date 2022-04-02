0327c247524f9b44b3dbfae12c593312e1223e35
KandaTeach <www.keneth.com.ph@gmail.com>
2022-04-03 03:18:00 +0800

    change query from log in repository

--- TripleJPMVPLibrary/Repository/LogInRepo.cs
+++ TripleJPMVPLibrary/Repository/LogInRepo.cs
@@ -16,8 +16,7 @@ namespace TripleJPMVPLibrary.Repository
             {               
                 con.Open();
                 // kini kay constant mani siya so PascalCase gamiton instead nga camelCase
-                const string SqlQuery = "Select username,password from administrator " +
-                                        "where username = @Username and password = @Password";
+                const string SqlQuery = "Select username, password from user_system_account where username = @Username and password = @Password";
                 var sqlCommand = new MySqlCommand(SqlQuery, con);
                 sqlCommand.Parameters.AddWithValue("@Username", username);
                 sqlCommand.Parameters.AddWithValue("@Password", password);
