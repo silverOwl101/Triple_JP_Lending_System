10d6e6b8805d28781b814d7d2f80c6842d3d4b72
exxxcube <johnexxxcube@gmail.com>
2023-04-09 03:06:03 +0800

    Database backup updated

--- DB/TjpDbBack.sql
+++ DB/TjpDbBack.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `tjpdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tjpdb`;
--- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tjpdb
 -- ------------------------------------------------------
--- Server version	8.0.31
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -527,15 +527,42 @@ DELIMITER ;
 /*!50003 SET character_set_results = utf8mb4 */ ;
 /*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getTotalCollection`(
 	IN customerUid char(36)
 )
 BEGIN
-	select c.id,c.name,sum(cl.returned) as totalCollection from collection as cl
+	select c.id,c.name,sum(cl.amount) as totalCollection from collection as cl
 	inner join customer_account as c on cl.customer_uid = c.uid
-    where cl.customer_uid = customerUid;
+    where cl.customer_uid = customerUid
+    group by c.id, c.name;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_getTotalCollectionPerLoanSummary` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getTotalCollectionPerLoanSummary`(
+
+)
+BEGIN	
+	select loan.id as 'ID',c.name as 'Name_of_the_Owner',
+    loan.principal_loan + (loan.principal_loan * 0.2)  as 'Released_Amount_with_Interest',
+    Sum(cl.amount) as 'Total_Collection' from collection as cl
+	left join customer_account as c on cl.customer_uid = c.uid
+    left join loan_information as loan on cl.loan_information_uid = loan.uid
+    group by loan.id;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -816,4 +843,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-01  1:13:05
+-- Dump completed on 2023-04-09  3:05:29
