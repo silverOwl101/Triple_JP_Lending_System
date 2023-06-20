c4a2a19437d6b38592a983d2f7dce7addfdbadb3
KandaTeach <www.keneth.com.ph@gmail.com>
2023-06-21 02:32:38 +0800

    adding config file

---
+++ TripleJPUtilityLibrary/ConfigFolder/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleJPUtilityLibrary.ConfigFolder
+{
+    internal class AppConfig
+    {
+        [DefaultValue("")]
+        public string DATABASE_CONNECTION_STRING { get; set; }
+    }
+}
